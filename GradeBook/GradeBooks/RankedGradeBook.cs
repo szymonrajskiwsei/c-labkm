b4467dad1bef3df6ca0d0d047c1d25a218f769a1
szymonrajskiwsei <72198126+szymonrajskiwsei@users.noreply.github.com>
2020-10-25 01:26:48 +0200

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -35,5 +35,25 @@ namespace GradeBook.GradeBooks
                 base.CalculateStudentStatistics(name);
             }
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade >= 60)
+                return 'B';
+            else if (averageGrade >= 40)
+                return 'C';
+            else if (averageGrade >= 20)
+                return 'D';
+            else
+                return 'F';
+
+        }
     }
 }
